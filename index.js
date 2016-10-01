0ff1c21544eecaa24c47c2a8b509105ee3183d22
Kurt Lee <breath103@gmail.com>
2016-10-01 21:23:30 +0900

    add really simple image transform with jump

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("working");
+const Jimp = require('jimp');
+// Test
+Jimp.read("./sample_images/Lea_Seydoux.jpg", function (err, lenna) {
+    if (err) throw err;
+    lenna.resize(256, 256)            // resize
+         .quality(60)                 // set JPEG quality
+         .greyscale()                 // set greyscale
+         .write("./Lea_Seydoux-result.jpg", (err, result) => {
+           if (err) throw err;
+           else {
+             console.log("Done : ", result);
+           }
+         }); // save
+});
