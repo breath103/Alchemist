8a1ad1b9a72447be863cc209f4eeacdff63b4f69
Kurt Lee <breath103@gmail.com>
2016-10-01 23:41:27 +0900

    fix script

--- src/index.ts
+++ src/index.ts
@@ -10,37 +10,28 @@ function readJimp(path) : Promise<Jimp> {
   });
 }
 
-function metamorphose(metamorphosis: Metamorphosis[]) : Promise<Jimp> {
+function writeJimp(image: Jimp, path: string) : Promise<Jimp> {
+  return new Promise((resolve, reject) => {
+    image.write(path, (err, result) => {
+      if (err) reject(err);
+      else resolve(result);
+    });
+  })}
 
+function metamorphose(image: Jimp, metamorphosis: Metamorphosis[]) : Promise<Jimp> {
+  let result = Promise.resolve(image);
+  metamorphosises.forEach((m) => {
+    result = result.then((transformed) => m.metamorphose(transformed));
+  });
+  return result;
 }
 
 const metamorphosises = [
   new CropMetamorphosis({x: 200, y: 200, width: 100, height: 100})
 ];
 
+
+
 readJimp("./sample_images/Lea_Seydoux.jpg")
-  .then((image) => {
-    let result = Promise.resolve(image);
-    metamorphosises.forEach((m) => {
-      result = result.then((transformed) => m.metamorphose(transformed));
-    });
-    return result;
-  })
-  .then(image => new Promise((resolve, reject) => {
-    image.write("./Lea_Seydoux-result.jpg", (err, result) => {
-      if (err) reject(err);
-      else resolve(result);
-    });
-  }), (err) => {
-    throw err;
-  });
-// if (err) throw err;
-// lenna.resize(256, 256)            // resize
-//      .quality(60)                 // set JPEG quality
-//      .greyscale()                 // set greyscale
-//      .write("./Lea_Seydoux-result.jpg", (err, result) => {
-//        if (err) throw err;
-//        else {
-//          console.log("Done : ", result);
-//        }
-//      }); // save
+  .then(image => metamorphose(image, metamorphosises))
+  .then(image => writeJimp(image, "./Lea_Seydoux-result.jpg"));
