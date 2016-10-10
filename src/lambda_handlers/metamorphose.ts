69b07ee5e6dc1fcd1e674be8668d728ce5917f9e
Kurt Lee <breath103@gmail.com>
2016-10-10 15:55:20 +0900

    added upload

--- src/lambda_handlers/metamorphose.ts
+++ src/lambda_handlers/metamorphose.ts
@@ -2,25 +2,13 @@ import { loadMetamorphosis } from '../metamorphosis';
 
 import ImageHelper from '../image_helper';
 
-// const SAVE_PATH = "./sample_images/Lea_Seydoux.jpg";
-// const RESULT_PATH = "./Lea_Seydoux-result.jpg";
-//
-// async function test() {
-//   console.log(testMetamorph);
-//   let image = await ImageHelper.readJimp(SAVE_PATH);
-//   image     = await testMetamorph.metamorphose(image);
-//   await ImageHelper.writeJimp(image, RESULT_PATH);
-//   console.log(`===============================\n check ${RESULT_PATH} for result\n\n`);
-//   return image;
-// }
-
 class Engine {
   private imageS3Bucket: string;
   private imageS3Prefix: string;
 
   constructor() {
-    this.imageS3Bucket = "s3-alchemist";
-    this.imageS3Prefix = "images/";
+    this.imageS3Bucket = 'alchemist-image';
+    this.imageS3Prefix = 'images/';
   }
 
   async metamorphose(imageId: string, metamorphosisRecipe: any) {
@@ -39,7 +27,7 @@ class Engine {
 const engine = new Engine();
 
 export default (event, context, cb) => {
-  const imageId = "12345"
+  const imageId = 'test-id'
   engine.metamorphose(
     imageId,
     require('../db/test.json')
@@ -48,17 +36,4 @@ export default (event, context, cb) => {
   }).catch(e => {
     cb(e);
   })
-  // test().then((result) => {
-  //   console.log(result);
-  //   cb(
-  //     null,
-  //     {
-  //       result,
-  //       message: 'Go Serverless v1.0! Your function executed successfully!',
-  //       event
-  //     }
-  //   );
-  // }).catch(e => {
-  //   cb(e);
-  // })
 };
---
+++ src/lambda_handlers/upload.ts
@@ -0,0 +1,27 @@
+import { S3 } from 'aws-sdk';
+
+async function uploadToS3(imageId:string, imageBody:Buffer) {
+  return new Promise((resolve, reject) => {
+    const s3 = new S3();
+
+    s3.upload({
+      Bucket: 'alchemist-image',
+      Key: 'images/',
+      Body: imageBody,
+    }, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
+}
+
+export default (event, context, cb) => {
+  // console.log(event);
+  cb(null, event);
+  // uploadToS3("test-id", event.body.image)
+  //   .then(r => {
+  //     cb(null, r);
+  //   }).catch(e => {
+  //     cb(e)
+  //   });
+};
